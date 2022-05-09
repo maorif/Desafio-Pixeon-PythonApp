ce0822b301a62271ff273f809153eb1ce538ee14
Maori <maorif.dev@gmail.com>
2022-05-08 23:55:19 -0300

    fix: comunicação entre api e banco + instruções no README

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-Desafio pixeon pythonApp utilizando fastAPI e postgreSQL
+Desafio pixeon pythonApp desenvolvido utilizando fastAPI e postgreSQL
+
+Inicialização:
+    $ docker-compose up
+
+A criação inicial do banco pode ser feita com:
+    $ python database/initial_db.py
+
+Rodando em: 
+    http://0.0.0.0:8000
+
+Para testar API
+    http://0.0.0.0:8000/docs
+
 
--- database/config.py
+++ database/config.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-database_url = "postgresql+asyncpg://postgres:123456@0.0.0.0:5432/fastAPI_app_db"
+database_url = "postgresql+asyncpg://postgres:123456@postgres/fastAPI_app_db"
 database_engine = create_async_engine(database_url)
 database_async_session = sessionmaker(bind=database_engine, class_=AsyncSession)
